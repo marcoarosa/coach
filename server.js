7f43d46f1cffaff68fefa88cdbc947453aea26b9
Marco Rosa <marco@marcopro.attlocal.net>
2018-08-20 15:19:56 -0400

    adding site basic layout

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const hbs = require('hbs')
+const fs = require('fs')
+const port = process.env.PORT || 3000
+
+var app = express()
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine','hbs') //handle bars
+
+app.use((req, res, next) => {  // Middleware to log 
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`
+    console.log(log);
+
+    //Create log file in server and append info
+    fs.appendFile('server.log',log + '\n',(err) => {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    })
+    next()
+})
+
+// app.use((req,res,next) => {
+//     res.render('maintenance.hbs');
+// })
+
+app.use(express.static(__dirname + '/public'));  //middleware
+
+//Helper with no variables
+hbs.registerHelper('getCurrentYear',() => {
+    return new Date().getFullYear()
+})
+
+//Helper with variables
+hbs.registerHelper('screamIt',(text) => {
+    return text.toUpperCase()
+})
+
+app.get('/',(req,res) => {
+    res.render('home.hbs',{
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my Site!'
+    })
+});
+
+app.get('/about',(req,res) => {
+    res.render('about.hbs',{
+        pageTitle: 'About Page'    })
+});
+
+app.get('/bad',(req,res) => {
+   res.send({
+       error: 'bad'
+   });
+});
+
+app.get('/projects',(req,res) => {
+    res.render('projects.hbs',{
+        pageTitle: 'Projects Page',
+        welcomeMessage: 'Projects will go here'
+    });
+});
+
+app.listen(port,() => {
+    console.log(`server is up on port ${port}.`);
+});
